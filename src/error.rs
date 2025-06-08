045f45a956809325a2c48c46d25dbc5c33e89450
Takagi Tasuku <tasuren@outlook.jp>
2025-06-08 11:37:40 +0900

    Add documentation comments to `Error`

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,11 @@
+/// Error types for window-getter-rs.
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
+    /// The error that occurs when the window environment is not found.
+    /// This can happen on only macOS.
     #[error("No window environment is running.")]
     NoWindowEnvironment,
+    /// platform-specific error that can occur when interacting with the window environment.
     #[error("A platform-specific error occurred: {0}")]
     PlatformSpecificError(#[from] super::platform_impl::PlatformError),
 }
